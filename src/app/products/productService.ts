787efe8dddf8de861913ee4be1fe91c37bd097e5
Gabriel Ponciano <gabrielponciano01@gmail.com>
2024-10-03 11:50:34 -0300

    0,1

--- src/app/products/productService.ts
+++ src/app/products/productService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';  // Certifique-se de importar Subject do RxJS
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, Subject, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';  // Importamos o 'tap' para emitir o evento e o 'catchError' para tratamento de erros
 
 export interface Product {
   id: number;
@@ -21,27 +22,49 @@ export interface Product {
 export class ProductService {
   private apiUrl = 'http://localhost:8080/products';
 
-  // Inicializa o Subject para emitir eventos de atualização da lista de produtos
-  private productListUpdated = new Subject<void>();  // Corrige a inicialização
+  // Subject para emitir eventos de atualização da lista de produtos
+  private productListUpdated = new Subject<void>();
 
   constructor(private http: HttpClient) {}
 
   // Método para obter produtos da API
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+    return this.http.get<Product[]>(this.apiUrl)
+      .pipe(
+        catchError(this.handleError)  // Adiciona o tratamento de erros
+      );
   }
 
   // Método para salvar um novo produto
   saveProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.apiUrl, product);
+    return this.http.post<Product>(this.apiUrl, product)
+      .pipe(
+        tap(() => this.refreshProductList()),  // Emite o evento de atualização
+        catchError(this.handleError)
+      );
   }
-  deleteProduct(id: number): Observable<void>  {
+
+  // Método para deletar um produto
+  deleteProduct(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`)
+      .pipe(
+        tap(() => this.refreshProductList()),  // Emite o evento de atualização
+        catchError(this.handleError)
+      );
+  }
+
+  // Método para atualizar um produto
+  updateProduct(updatedProduct: Product, productId: number): Observable<Product> {
+    return this.http.put<Product>(`${this.apiUrl}/${productId}`, updatedProduct)
+      .pipe(
+        tap(() => this.refreshProductList()),  // Emite o evento de atualização
+        catchError(this.handleError)
+      );
   }
 
   // Emite o evento para informar que a lista de produtos foi atualizada
   refreshProductList(): void {
-    this.productListUpdated.next();  // Aqui o Subject é inicializado corretamente, então podemos chamar .next()
+    this.productListUpdated.next();  // Emite o evento de atualização
   }
 
   // Inscreve-se nas atualizações da lista de produtos
@@ -49,8 +72,17 @@ export class ProductService {
     return this.productListUpdated.asObservable();
   }
 
-  updateProduct(updatedProduct: Product, productId: number): Observable<Product> {
-    return this.http.put<Product>(`${this.apiUrl}/${productId}`, updatedProduct);
+  // Função para tratamento de erros
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'Ocorreu um erro desconhecido!';
+    if (error.error instanceof ErrorEvent) {
+      // Erro no lado do cliente
+      errorMessage = `Erro: ${error.error.message}`;
+    } else {
+      // Erro no lado do servidor
+      errorMessage = `Código do erro: ${error.status}\nMensagem: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));  // Lança o erro
   }
-
 }
--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ProductService, Product } from '../productService';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { EditProductDialogComponent } from './edit-product-dialog/edit-product-dialog.component';
@@ -11,18 +11,19 @@ import { EditProductDialogComponent } from './edit-product-dialog/edit-product-d
   standalone: true,
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, ReactiveFormsModule, MatDialogModule]
 })
 export class ProductListComponent implements OnInit {
-  products: Product[] = [];  // Todos os produtos
-  filteredProducts: Product[] = [];  // Produtos filtrados
-  paginatedProducts: Product[] = [];  // Produtos paginados
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  paginatedProducts: Product[] = [];
   currentPage: number = 1;
   productsPerPage: number = 3;
   searchForm!: FormGroup;
-
-  // Propriedade para controlar a exibição da barra de pesquisa
-  showSearchBar: boolean = false;  // Adicionada para controlar a visibilidade da barra de pesquisa
+  showSearchBar: boolean = false;
+  loading: boolean = false;
+  errorMessage: string = '';
 
   constructor(
     private fb: FormBuilder,
@@ -36,69 +37,72 @@ export class ProductListComponent implements OnInit {
 
     // Escuta as atualizações de produtos
     this.productService.onProductListUpdated().subscribe(() => {
-      this.loadProducts();  // Recarrega os produtos quando necessário
+      this.loadProducts();
     });
   }
 
-  // Inicializa o formulário de busca
+  // Inicializa o formulário de pesquisa
   initializeForm(): void {
     this.searchForm = this.fb.group({
-      searchTerm: ['']
+      searchTerm: ['', Validators.required]
     });
 
-    // Filtra produtos a cada mudança no campo de busca
     this.searchForm.get('searchTerm')?.valueChanges.subscribe(searchTerm => {
-      this.filterProducts(searchTerm);  // Atualiza produtos dinamicamente
+      this.filterProducts(searchTerm);
     });
   }
 
-  // Função para alternar a exibição da barra de pesquisa
-  toggleSearch(): void {
-    this.showSearchBar = !this.showSearchBar;  // Alterna entre true e false
-  }
-
-  // Carrega os produtos e atualiza a lista paginada
+  // Carregar produtos
   loadProducts(): void {
-    this.productService.getProducts().subscribe((products: Product[]) => {
-      this.products = products.map(product => ({ ...product, showActions: false }));
-      this.filterProducts(this.searchForm.get('searchTerm')?.value || ''); // Garante que a filtragem inicial seja aplicada
-    });
+    this.loading = true;
+    this.productService.getProducts().subscribe(
+      (products: Product[]) => {
+        this.products = products.map(product => ({ ...product, showActions: false }));
+        this.filterProducts(this.searchForm.get('searchTerm')?.value || '');
+        this.loading = false;
+      },
+      error => {
+        this.handleError(error);
+        this.loading = false;
+      }
+    );
   }
 
-  // Função de filtragem de produtos
+  // Filtra produtos com base no termo de pesquisa
   filterProducts(searchTerm: string): void {
     if (searchTerm) {
       this.filteredProducts = this.products.filter(product =>
-        Object.values(product).some(value => {
-          // Verifica se o valor não é nulo ou indefinido antes de usar toString()
-          return value !== null && value !== undefined && value.toString().toLowerCase().includes(searchTerm.toLowerCase());
-        })
+        Object.values(product).some(value =>
+          value?.toString().toLowerCase().includes(searchTerm.toLowerCase())
+        )
       );
     } else {
-      this.filteredProducts = this.products;  // Mostra todos os produtos se o campo de busca estiver vazio
+      this.filteredProducts = this.products;
     }
-    this.updatePaginatedProducts();  // Atualiza a lista paginada
+    this.updatePaginatedProducts();
   }
 
-
-  // Atualiza a lista de produtos paginada
+  // Atualiza a lista de produtos paginados
   updatePaginatedProducts(): void {
     const startIndex = (this.currentPage - 1) * this.productsPerPage;
     this.paginatedProducts = this.filteredProducts.slice(startIndex, startIndex + this.productsPerPage);
   }
 
-  // Muda para a página específica
+  // Muda a página atual
   changePage(page: number): void {
     this.currentPage = page;
     this.updatePaginatedProducts();
   }
 
-  // Retorna o número total de páginas
-  totalPages(): number {
-    return Math.ceil(this.filteredProducts.length / this.productsPerPage);
+  // Atualiza o número de itens por página
+  updateItemsPerPage(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    this.productsPerPage = +selectElement.value;
+    this.updatePaginatedProducts();
   }
 
-  // Abre o diálogo de confirmação de exclusão
+
+  // Confirmação de exclusão
   confirmDelete(productId: number): void {
     const dialogRef = this.dialog.open(ConfirmDialogComponent);
 
@@ -107,15 +111,17 @@ export class ProductListComponent implements OnInit {
     });
   }
 
-  // Deleta o produto e atualiza a lista
+  // Exclui um produto
   deleteProduct(productId: number): void {
-    this.productService.deleteProduct(productId).subscribe({
-      next: () => this.loadProducts(),
-      error: (err) => console.error(`Erro ao deletar o produto com ID ${productId}`, err)
-    });
+    this.productService.deleteProduct(productId).subscribe(
+      () => {
+        this.loadProducts();
+      },
+      error => this.handleError(error)
+    );
   }
 
-  // Abre o diálogo de edição de produto
+  // Abre o diálogo de edição
   openEditDialog(product: Product): void {
     const dialogRef = this.dialog.open(EditProductDialogComponent, {
       width: '400px',
@@ -127,10 +133,28 @@ export class ProductListComponent implements OnInit {
     });
   }
 
-  // Atualiza o produto e recarrega a lista
+  // Atualiza um produto
   updateProduct(updatedProduct: Product, productId: number): void {
-    this.productService.updateProduct(updatedProduct, productId).subscribe(() => {
-      this.loadProducts();
-    });
+    this.productService.updateProduct(updatedProduct, productId).subscribe(
+      () => {
+        this.loadProducts();
+      },
+      error => this.handleError(error)
+    );
+  }
+
+  // Tratamento de erros
+  handleError(error: any): void {
+    this.errorMessage = `Erro: ${error.statusText || 'Erro desconhecido'}`;
+  }
+
+  // Calcula o número total de páginas
+  totalPages(): number {
+    return Math.ceil(this.filteredProducts.length / this.productsPerPage);
+  }
+
+  // Alterna a barra de pesquisa
+  toggleSearch(): void {
+    this.showSearchBar = !this.showSearchBar;
   }
 }
